67c9a8aa3f165df4dd69f10ff0aa2463eb43ef71
Phạm Tuấn Anh <tagamer9669@gmail.com>
2026-08-08 11:15:35 +0700

    Loading Data with MVC

---
+++ twicher/ViewComponents/StoriesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Twicher.Data;
+
+namespace Twicher.ViewComponents
+{
+    public class StoriesViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public StoriesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allStories = await _context.Stories
+                .Where(n => n.DateCreated >= DateTime.UtcNow.AddHours(-24))
+                .Include(s => s.User)
+                .ToListAsync();
+
+            return View(allStories);
+        }
+    }
+}
